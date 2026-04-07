f5c9a549fd80c973e668fb771be8e88c415266dc
AdoGaric <ademirgaric@live.nl>
2026-04-07 18:33:09 +0200

    test2

---
+++ TransportBedrijfBackEnd/TransportBedrijfBackEnd/Program.cs
@@ -0,0 +1,9 @@
+﻿using TransportBedrijfBackEnd;
+
+var repo = new VoertuigRepository();
+var voertuigen = repo.GetAll();
+
+foreach (var voertuig in voertuigen)
+{
+    Console.WriteLine($"ID: {voertuig.VoertuigId}, Kenteken: {voertuig.Kenteken}, Type: {voertuig.Type}");
+}
---
+++ TransportBedrijfBackEnd/TransportBedrijfBackEnd/VoertuigRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TransportBedrijfBackEnd
+{
+    internal class VoertuigRepository
+    {
+        private string connectionString = "Server=127.0.0.1;Port=3306;Database=transportbedrijf;Uid=root;Pwd=root;";
+
+        public List<Voertuig> GetAll()
+        {
+            List<Voertuig> voertuigen = new List<Voertuig>();
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Voertuig";
+                var command = new MySqlCommand(query, connection);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Voertuig v = new Voertuig();
+                    v.VoertuigId = Convert.ToInt32(reader["voertuig_id"]);
+                    v.Kenteken = reader["kenteken"].ToString();
+                    v.Type = reader["type"].ToString();
+                    v.Kilometerstand = Convert.ToDecimal(reader["kilometerstand"]);
+                    v.Afschrijving = Convert.ToDecimal(reader["afschrijving"]);
+                    voertuigen.Add(v);
+                }
+            }
+
+            return voertuigen;
+        }
+    }
+}
